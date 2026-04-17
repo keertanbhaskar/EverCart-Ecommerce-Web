ac5fe707c710de728f43fe8d11f4ed17e1fbd562
keertanbhaskar <keertanbhaskar@gmail.com>
2026-04-18 01:48:42 +0530

    feat: refactor multer config and implement cloudinary image upload in product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,53 +1,64 @@
-
+import { v2 as cloudinary } from "cloudinary";
 
 // function for add product
 
-const addProduct = async(req,res)=>{
-try {
-  const {name,description,price,category,subCategory,sizes,bestseller} = req.body
+const addProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    } = req.body;
 
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
 
-  const image1 = req.files.image1[0]
-  const image2 = req.files.image2[0];
-  const image3 = req.files.image3[0];
-  const image4 = req.files.image4[0];
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined,
+    );
 
-  
-  console.log(
-    name,
-    description,
-    price,
-    category,
-    subCategory,
-    sizes,
-    bestseller,
-  );
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      }),
+    );
 
-  console.log(image1,image2,image3,image4);
+    console.log(
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    );
 
-  res.json({})
+    console.log(imagesUrl);
 
-} catch (error) {
-  console.log(error);
-  
-  res.json({success:false,message:error.message})
-}
+    res.json({});
+  } catch (error) {
+    console.log(error);
 
-}
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for list product
-const listProducts = async(req,res)=>{
-
-}
+const listProducts = async (req, res) => {};
 
 // function for removing product
-const removeProduct = async (req,res)=>{
-
-}
+const removeProduct = async (req, res) => {};
 
 // function for single product info
-const singleProduct = async(req,res) => {
+const singleProduct = async (req, res) => {};
 
-}
-
-export {listProducts,addProduct,removeProduct,singleProduct}
+export { listProducts, addProduct, removeProduct, singleProduct };
